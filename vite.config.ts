962694da9cbfe2d08f16e0f6f6935bca2838a336
cherrychen <820769835@qq.com>
2022-10-10 16:16:59 +0800

    config: 增加部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default function ({ command }: ConfigEnv): UserConfigExport {
   const root = process.cwd();
   return {
     root,
+    base: './',
     resolve: {
       alias: [
         {
